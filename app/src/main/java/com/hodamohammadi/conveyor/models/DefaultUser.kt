0c0ff7637e185402625e41245363c904d3009ae4
Hoda Mohamamdi <mohamh8@mcmaster.ca>
2019-03-02 16:49:27 -0500

    Make message and user models serializable

--- app/src/main/java/com/hodamohammadi/conveyor/models/DefaultUser.kt
+++ app/src/main/java/com/hodamohammadi/conveyor/models/DefaultUser.kt
@@ -1,22 +1,43 @@
 package com.hodamohammadi.conveyor.models
 
 import com.stfalcon.chatkit.commons.models.IUser
+import java.io.Serializable
 
 /**
  * Default model for a user.
  */
-class DefaultUser constructor(private val id: String, private val name: String?, private val avatar: String?): IUser {
+class DefaultUser: IUser, Serializable {
+    private var id: String
+    private var name: String?
+    private var avatar: String?
+
+    constructor(id: String, name: String?, avatar: String?) {
+        this.id = id
+        this.name = name
+        this.avatar = avatar
+    }
+
+    fun setId(id: String) {
+        this.id = id
+    }
 
     override fun getId(): String {
         return id
+    }
+
+    fun setName(name: String) {
+        this.name = name
     }
 
     override fun getName(): String? {
         return name
     }
 
+    fun setAvatar(avatar: String) {
+        this.avatar = avatar
+    }
+
     override fun getAvatar(): String? {
         return avatar
     }
-
 }
