6286f94f9c7e6237a8e2de0049596c2b90a19458
Hoda Mohamamdi <mohamh8@mcmaster.ca>
2019-03-02 21:06:30 -0500

    parcelize models

--- conveyor/src/main/java/com/hodamohammadi/conveyor/models/DefaultUser.kt
+++ conveyor/src/main/java/com/hodamohammadi/conveyor/models/DefaultUser.kt
@@ -1,43 +1,26 @@
 package com.hodamohammadi.conveyor.models
 
+import android.os.Parcelable
 import com.stfalcon.chatkit.commons.models.IUser
-import java.io.Serializable
+import kotlinx.android.parcel.Parcelize
 
 /**
  * Default model for a user.
  */
-class DefaultUser: IUser, Serializable {
-    private var id: String
-    private var name: String?
-    private var avatar: String?
-
-    constructor(id: String, name: String?, avatar: String?) {
-        this.id = id
-        this.name = name
-        this.avatar = avatar
-    }
-
-    fun setId(id: String) {
-        this.id = id
-    }
+@Parcelize
+data class DefaultUser(private val userId: String,
+                       private val userName: String?,
+                       private val userAvatar: String?) : Parcelable, IUser {
 
     override fun getId(): String {
-        return id
-    }
-
-    fun setName(name: String) {
-        this.name = name
+        return userId
     }
 
     override fun getName(): String? {
-        return name
-    }
-
-    fun setAvatar(avatar: String) {
-        this.avatar = avatar
+        return userName
     }
 
     override fun getAvatar(): String? {
-        return avatar
+        return userAvatar
     }
 }
