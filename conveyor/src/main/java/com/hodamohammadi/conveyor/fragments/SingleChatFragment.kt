f632d893f8658094f525efea77cc94b3250655c3
hodamohammadi <mohamh8@mcmaster.ca>
2019-03-09 20:14:45 -0500

    create view model factory singleton

--- conveyor/src/main/java/com/hodamohammadi/conveyor/fragments/SingleChatFragment.kt
+++ conveyor/src/main/java/com/hodamohammadi/conveyor/fragments/SingleChatFragment.kt
@@ -1,5 +1,6 @@
 package com.hodamohammadi.conveyor.fragments
 
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +9,8 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import com.hodamohammadi.conveyor.utils.FirebaseHelper
 import com.hodamohammadi.conveyor.R
+import com.hodamohammadi.conveyor.viewmodels.ViewModelFactory
+import com.hodamohammadi.conveyor.viewmodels.ChatViewModel
 import com.stfalcon.chatkit.commons.ImageLoader
 import com.stfalcon.chatkit.commons.models.IMessage
 import com.stfalcon.chatkit.messages.MessageInput
@@ -21,6 +24,8 @@ class SingleChatFragment : Fragment(), MessageInput.InputListener, MessageInput.
         MessageInput.TypingListener {
 
     private lateinit var messagesAdapter: MessagesListAdapter<IMessage>
+    private lateinit var chatViewModel: ChatViewModel
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,6 +40,11 @@ class SingleChatFragment : Fragment(), MessageInput.InputListener, MessageInput.
         input.setInputListener(this)
         input.setTypingListener(this)
         input.setAttachmentsListener(this)
+
+        val viewModelFactory = ViewModelFactory.INSTANCE
+        chatViewModel = ViewModelProviders.of(requireActivity(), viewModelFactory)
+                .get(ChatViewModel::class.java)
+
         initAdapter()
     }
 
---
+++ conveyor/src/main/java/com/hodamohammadi/conveyor/viewmodels/ViewModelFactory.kt
@@ -0,0 +1,23 @@
+package com.hodamohammadi.conveyor.viewmodels
+
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProvider
+
+
+/**
+ * singleton of view model factory to create instances of view models.
+ */
+class ViewModelFactory private constructor(): ViewModelProvider.Factory {
+    companion object {
+        val INSTANCE = ViewModelFactory()
+    }
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return if (modelClass.isAssignableFrom(ChatViewModel::class.java)) {
+            ChatViewModel() as T
+        } else {
+            throw IllegalArgumentException("ViewModel Not Found")
+        }
+    }
+
+}
